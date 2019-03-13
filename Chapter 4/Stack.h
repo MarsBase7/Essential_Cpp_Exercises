7c4771f8a504eb35995c66c2e44161006e87d3d9
MarsBase7 <33893050+MarsBase7@users.noreply.github.com>
2019-03-13 15:38:35 +0800

    Add files via upload

---
+++ Chapter 4/Stack.h
@@ -0,0 +1,19 @@
+# include <string>
+# include <vector>
+# include <iostream>
+
+using namespace std;
+
+class Stack
+{
+public:
+	bool push( const string & );
+	bool pop( string &elem );
+	bool peek( string &elem );
+	bool empty() const { return _stack.empty(); }
+	bool full() const { return _stack.size() == _stack.max_size(); }
+	int size() const { return _stack.size(); }
+	
+private:
+	vector<string> _stack;
+};
